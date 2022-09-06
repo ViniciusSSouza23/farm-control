56745b3c0f08957c738c6a6636b667b75b845bc0
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-09-06 14:42:20 -0300

    init home :construction:

---
+++ src/components/home/ContentArea.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content-area-component container pt-4">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <h2 class="text-center">Estatisticas da sua propriedade:</h2>
+        
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.content-area-component{
+  background-color: rgb(209, 209, 209);
+  height: 40vh;
+}
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,38 @@
 <template>
-<div class="home-view pt-5">
-  
-</div>
+  <div class="home-view">
+    <section class="img d-flex align-items-center justify-content-center">
+      <h1>Bem vindo {{ user }} ao Farm Controller</h1>
+    </section>
+    <content-area/>
+   
+  </div>
 </template>
+<script>
+import ContentArea from "@/components/home/ContentArea.vue"
+export default {
+  data() {
+    return {
+      user: "Vinicius",
+    };
+  },
+  components:{
+    ContentArea
+  }
+};
+</script>
+<style lang="scss" scoped>
+.home-view {
+  .img {
+    background-image: url(../assets/img/home/farm.jpg);
+    width: 100%;
+    min-height: 300px;
+    background-position-x: center;
+    background-position-y: bottom;
+    /* h1 {
+      mix-blend-mode: difference;
+      filter: invert(1);
+    } */
+  }
+}
+</style>
 
