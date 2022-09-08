63e7f748b820fcb9181f8b766d126d1c2837d08b
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-09-08 15:19:44 -0300

    :construction:

--- src/views/BreedersView.vue
+++ src/views/BreedersView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="breeders-view">
-    Tela de Matrizes
+  <div class="breeders-view py-5">
+    <div class="container">
+      <div class="row w-100 justify-content-end">
+        <div class="col-lg-6">
+          <h2 class="text-center">Bem vindo ao gerenciamento de matrizes do seu rebanho</h2>
+          <div class="d-flex pt-5 flex-column align-items-center">
+            <button class="btn btn-primary"> Cadastrar Matrizes</button>
+            <button class="btn btn-primary my-4">Consultar Matrizes</button>
+            <button class="btn btn-danger">Deletar Matrizes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style lang='scss' scoped>
+.breeders-view{
+  background: url(../assets/img/breeders/breeders-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: 50% 100%;
+  min-height:80vh;
+}
+</style>
